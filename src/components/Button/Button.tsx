d864a7d2f2db2be103fbcc40d4f01a462493d65c
Anton Gatsko <anton.gatsko@cloudcall.com>
2022-03-29 14:24:27 +0200

    Initial commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React, {FC, ReactElement} from 'react'
+import styles from './Button.module.css';
+
+interface ButtonProps{
+    label: string
+    onClick: () => void
+    icon: ReactElement
+}
+
+const Button: FC<ButtonProps> = ({label, onClick, icon}) => {
+    return <button className={styles.button} onClick={onClick}>{icon}{label}</button>
+}
+export default Button
---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dropdown = () => {
+    return <div>
+        Test
+    </div>;
+}
+
+export default Dropdown;
