669476351b24468b5db9e00d71c21f20f45bc069
Anton Gatsko <anton.gatsko@cloudcall.com>
2022-03-31 17:52:11 +0200

    Hook update

--- src/components/FilterButton/FilterButton.tsx
+++ src/components/FilterButton/FilterButton.tsx
@@ -1,30 +1,20 @@
 import Button from "../Button/Button";
 import {ReactComponent as Plus} from "../Icons/plus.svg";
 import Dropdown from "../Dropdown/Dropdown";
-import React, {useEffect, useRef, useState} from "react";
+import React, { useRef, useState} from "react";
 import styles from './FilterButton.module.css'
 import {FILTER_BUTTON_LABEL} from "../../consts";
+import {useClickOutside} from "../../hooks/useClickOutside";
 
 const FilterButton = () => {
     const [isDropdownVisible, setIsDropdownVisible] = useState(false);
-    const ref = useRef(null);
+    const ref = useRef<HTMLDivElement>(null);
 
     const handleButtonClick = () => {
         setIsDropdownVisible(prevIsVisible => !prevIsVisible)
     }
 
-    useEffect(() => {
-        const handleClickOutside = (event: MouseEvent) => {
-            //@ts-ignore have to ignore here because for some reason can't type it right atm
-            if (ref.current && !ref.current.contains(event.target)) {
-                setIsDropdownVisible(false);
-            }
-        };
-        document.addEventListener('click', handleClickOutside, true);
-        return () => {
-            document.removeEventListener('click', handleClickOutside, true);
-        };
-    }, []);
+    useClickOutside(ref, () => setIsDropdownVisible(false));
 
     return <div ref={ref} className={styles.container}>
         <Button label={FILTER_BUTTON_LABEL} onClick={handleButtonClick} icon={<Plus/>}/>
